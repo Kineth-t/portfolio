77104369c9b73979afeb72f50f6f9b2904f59df5
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-20 22:54:10 +0800

    Set up some projects in project tabs. Next step will be to animate them when scrolling the website.

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -5,14 +5,16 @@ export default function Projects() {
     return (
         <div className="projects-container">
             <h1>Projects</h1>
-            <h3>Here are some of the projects that I did in school or on my free time!</h3>
+            <h3>Here are some of the projects that I did in school or on my free time</h3>
             <div className='project-cards'>
-                {projectData.map(project =>
-                    <div className="project-card" key={project.name}>
-                        <h3>{project.name}</h3>
-                        <img src={project.image} alt="Project's Website homepage"/>
-                        <p>{project.description}</p>
-                        <p>{project.stack}</p>
+                {projectData.map((project, index) =>
+                    <div className="project-card" key={project.name} style={{'--position': index, zIndex: 10 - index}}>
+                        <div className="project-details">
+                            <h3>{project.name}</h3>
+                            <p><b>Description:</b> {project.description}</p>
+                            <p><b>Stack:</b> {project.stack}</p>
+                        </div>
+                        <img src={project.image} alt="Project's Website homepage" style={{'--position': index, zIndex: 10 - index}}/>
                     </div>
                 )}
             </div>
