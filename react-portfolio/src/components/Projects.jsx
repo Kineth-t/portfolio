b35a46d7fcb8ebb8f8ef473f0c507afd759b4760
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-21 20:09:08 +0800

    Style the header with gradient background colour and utilise typerwriter library.

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -4,8 +4,11 @@ import projectData from './projects.json';
 export default function Projects() {
     return (
         <div className="projects-container">
-            <h1>Projects</h1>
-            <h3>Here are some of the projects that I did in school or on my free time</h3>
+            <div className="project-head">
+                <h1>Projects</h1>
+                <img src="./src/assets/project-icon.png" alt="project icon" />
+            </div>
+            <h3>Here are some of the projects that I did in school or on my free time.</h3>
             <div className='project-cards'>
                 {projectData.map((project, index) =>
                     <div className="project-card" key={project.name} style={{'--position': index, zIndex: 10 - index}}>
--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import About from '../components/About'
 import Projects from '../components/Projects'
+import { Typewriter } from 'react-simple-typewriter'
 import './Home.css'
 
 export default function HomePage() {
     return (
         <>  
             <header className="intro">
-                <h1 className="main-headline">Hello welcome to my portfolio website</h1>
+                <h1 className="main-headline">Hello, I'm Kenneth Tay.</h1>
                 <div className="my-photo-container"><img className='my-photo' src='./src/img/me.jpeg'/></div>
-                <h2 className="headline-text">Hello welcome to my portfolio website</h2>
+                <h2 className="headline-text">
+                    I'm{' '}
+                    <span className="typewriter">
+                        <Typewriter
+                            words={[
+                            'an aspiring developer.',
+                            'a problem solver.',
+                            'always curious.',
+                            'always learning.'
+                            ]}
+                            loop={true}
+                            cursor
+                            cursorStyle="|"
+                            typeSpeed={80}
+                            deleteSpeed={50}
+                            delaySpeed={1500}
+                        />
+                    </span>
+                </h2>
+                <h3 className='headline-subtext'>Exploring the world of technology and software development at Singapore Management Unicersity(SMU).</h3>
             </header>
             <main>
                 <About />
