42e78b1509a45c9baa7a7b5b14f0636e0cbb2687
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-23 15:33:40 +0800

    Completed "Experience" tab on the home page.

--- react-portfolio/src/components/Experiences.jsx
+++ react-portfolio/src/components/Experiences.jsx
@@ -1,9 +1,25 @@
 import './Experiences.css'
+import experienceData from './experiences.json'
 
 export default function Experiences() {
     return (
-        <div className="experiences-container">
-            <h1>Experiences</h1>
+        <div className="experience-tab">
+            <div className="experience-header">
+                <h1>Experience</h1>
+                <img src='./src/assets/experience.png'/>
+            </div>
+            <div className="experience-container">
+                {experienceData.map(exp =>
+                    <div className='experience' key={exp.description}>
+                        <h2>{exp.role}</h2>
+                        <h3>{exp.company}</h3>
+                        <p>{exp.description}</p>
+                    </div>
+                )}
+            </div>
+            <div className="background">
+                <img src='./src/img/experience-background.jpg' alt="Testing" />
+            </div>
         </div>
     )
 }
