c8083cf528f87c917185c6e7011255d66e01034e
Kenneth Tay <kenneth21tay@gmail.com>
2025-08-26 22:23:05 +0800

    Completed certification section.

--- react-portfolio/src/components/Certifications.jsx
+++ react-portfolio/src/components/Certifications.jsx
@@ -7,5 +7,19 @@ export default function Certifications() {
             <h1>Certifications</h1>
             <img src="./src/assets/certs.png" />
         </div>
+            <div className="cert-cards-container">
+                {certifications.map(cert =>
+                    <div className="cert-card" key={cert.url}>
+                        <h3>{cert.name}</h3>
+                        <div className="organisation">
+                            <img src={cert.img} alt="Organisation logo" />
+                            <p>{cert.organisation}</p>
+                        </div>
+                        <span>Issued on: {cert.date}</span>
+                        {cert.expiry ? <span> - {cert.expiry}</span> : undefined}
+                        <div>Credential: <a href={cert.url} target='_blank'>{cert.url}</a></div>
+                    </div>
+                )}
+        </div>
     </div>
 }
